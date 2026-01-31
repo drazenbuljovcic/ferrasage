4b66b92b3528ddf5871ae59852216ec4261e08f3
Dražen Buljovčić <drazen.buljovcic@gmail.com>
2026-01-31 20:47:54 +0100

    seo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import { Helmet } from "react-helmet-async";
 
 import {
   Header,
@@ -21,41 +20,6 @@ function HomePage() {
   return (
     <>
       <div className="min-h-screen bg-slate-900 text-slate-300">
-        <Helmet>
-          <title>Ferrasage | Engineering Expertise</title>
-          <meta name="robots" content="index, follow" />
-          <link rel="canonical" href="https://ferrasage.com/" />
-          <meta name="author" content="Ferrasage"></meta>
-          <meta
-            data-react-helmet="true"
-            name="description"
-            content="We are engineers by practice and professionals by principle — hands‑on, accountable, and committed to deliver. When challenges arise, we engage directly, with respect for the technology, the craft, and the people behind it."
-          />
-          <meta name="theme-color" content="#98a869" />
-
-          <meta property="og:type" content="website" />
-          <meta
-            property="og:title"
-            content="Ferrasage | Engineering Expertise"
-          />
-          <meta
-            property="og:description"
-            content="We are engineers by practice and professionals by principle — hands‑on, accountable, and committed to deliver. When challenges arise, we engage directly, with respect for the technology, the craft, and the people behind it."
-          />
-          <meta property="og:url" content="https://ferrasage.com/" />
-          <meta property="og:image" content="https://ferrasage.com/og.png" />
-
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="twitter:title"
-            content="Ferrasage | Engineering Expertise"
-          />
-          <meta
-            name="twitter:description"
-            content="We are engineers by practice and professionals by principle — hands‑on, accountable, and committed to deliver. When challenges arise, we engage directly, with respect for the technology, the craft, and the people behind it."
-          />
-          <meta name="twitter:image" content="https://ferrasage.com/og.png" />
-        </Helmet>
         <Header onNavigate={scrollToSection} />
 
         <main className="mt-12">
